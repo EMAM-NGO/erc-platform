351bdb7f00208b25a7568f953e154b0e327ce7be
EMAM-NGO <emam.org.ngo@gmail.com>
2025-09-20 02:53:00 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import monacoEditorPlugin from 'vite-plugin-monaco-editor'
+import monacoEditorPlugin from 'vite-plugin-monaco-editor-esm'  // Changed import
 
 // https://vitejs.dev/config/
 export default defineConfig({
